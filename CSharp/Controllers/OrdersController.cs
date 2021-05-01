19d10f941df647497e03c68ad10516b5a3f5e1f3
ChrisWaggoner <82328541+ChrisWaggoner@users.noreply.github.com>
2021-04-30 20:36:26 -0500

    Added orders controller for basic endpoints.  Also added an endpoint to get orders based on a customer ID.  Also added unit tests based on mock methods that will need replaced with order service calls and new tests

---
+++ CSharp/Controllers/OrdersController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+//using System;
+using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CSharp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+
+    public class OrdersController : ControllerBase
+    {
+
+        // GET: api/<OrdersController>
+        // gets all of the orders
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Order1", "Order2" };
+        }
+
+        // GET api/<OrdersController>/5
+        // returns the order for this order id
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "Single Order";
+        }
+
+        // GET api/<OrdersController>/customer/5
+        // returns orders for this customer id
+        [HttpGet("customer/{id}")]
+        public IEnumerable<string> GetOrdersForCustomer(int id)
+        {
+            return new string[] { "CustomerOrder1", "CustomerOrder2" };
+        }
+
+        // POST api/<OrdersController>
+        // creates a new order
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            //create the new order here
+        }
+
+        // PUT api/<OrdersController>/5
+        // Used to update the order
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+            //update the order here
+        }
+
+        // DELETE api/<OrdersController>/5
+        // I would assume a delete would be used for a cancel.
+        // If not a new post method would be created with the payload
+        // for the cancel Information 
+        // Or this void method could be renamed to better reflect
+        // it's actions
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            //cancel the order here
+        }
+
+    }
+}
---
+++ CSharpTest/OrdersTest.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace OrdersTest
+{
+    public class OrdersTest
+    {
+        
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+
+        [Test]
+        public void OrdersGetReturnsAtLeastTwoOrders()
+        { 
+            CSharp.Controllers.OrdersController ordersTest = new CSharp.Controllers.OrdersController();
+            string[] result = (string[])ordersTest.Get();
+            Assert.That(result.Length, Is.AtLeast(2));
+        }
+
+        [Test]
+        public void GetOrdersByCustomerReturnsAtLeastTwoOrders()
+        {
+            CSharp.Controllers.OrdersController ordersTest = new CSharp.Controllers.OrdersController();
+            string[] result = (string[])ordersTest.GetOrdersForCustomer(3);
+            Assert.That(result.Length, Is.AtLeast(2));
+        }
+
+        [Test]
+        public void OrdersGetByIDReturnsSingleOrderForAnyOrderID()
+        {
+            CSharp.Controllers.OrdersController ordersTest = new CSharp.Controllers.OrdersController();
+            string result = ordersTest.Get(3);
+            Assert.That(result, Is.EqualTo("Single Order"));
+        }
+
+        [Test]
+        public void OrderPostAcceptsAStringWithoutError()
+        {
+            CSharp.Controllers.OrdersController ordersTest = new CSharp.Controllers.OrdersController();
+            ordersTest.Post("test");
+            Assert.True(true);
+        }
+
+        [Test]
+        public void OrderPutAcceptsAnIDAndStringWithoutError()
+        {
+            CSharp.Controllers.OrdersController ordersTest = new CSharp.Controllers.OrdersController();
+            ordersTest.Put(3, "test");
+            Assert.True(true);
+        }
+
+        [Test]
+        public void OrderDeleteAcceptsAnIDWithoutError()
+        {
+            CSharp.Controllers.OrdersController ordersTest = new CSharp.Controllers.OrdersController();
+            ordersTest.Delete(3);
+            Assert.True(true);
+        }
+    }
+}
